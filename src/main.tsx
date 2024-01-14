08059bf0ca890d8442ccde059c1114f236fbe3c9
Artem Lipatov <houndvibe@gmail.com>
2024-01-14 21:15:10 +0600

    redux and router added

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,24 @@
 import ReactDOM from "react-dom/client";
 import App from "./routes/App.tsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <>ErrorPage</>,
+  },
+  {
+    path: "1",
+    element: <>1</>,
+    errorElement: <>ErrorPage</>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
